4ac40effc5610eb26a43d16bb5ad0b394c4510ef
steve olsen <steve.olsen@close.com>
2023-09-12 15:37:55 -0500

    test out auth

---
+++ gameplay-computer/src/auth.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import { jwtVerify, importSPKI, errors } from "jose";
+import type { AstroCookies } from "astro";
+
+const publicKey = import.meta.env.CLERK_JWT_PUBLIC_KEY;
+
+const ClerkUser = z.object({
+    clerk_id: z.string(),
+    email: z.string(),
+    first_name: z.string(),
+    last_name: z.string(),
+    username: z.string(),
+  });
+
+export type ClerkUser = z.infer<typeof ClerkUser>;
+  
+export async function currentUser(cookies: AstroCookies): Promise<ClerkUser | null> {
+    const cookie = cookies.get("__session");
+    if (cookie && cookie.value) {
+        const token = cookie.value;
+        // todo perf: construct this at startup
+        const key = await importSPKI(publicKey, "RS256");
+        try {
+            const decoded = await jwtVerify(token, key);
+            const user = ClerkUser.parse(decoded.payload);
+            console.log(user);
+            return user;
+        } catch (e) {
+            if (e instanceof errors.JWTExpired) {
+                // todo: refresh token
+            } else {
+                console.error(e)
+            }
+        }
+    }
+    return null;
+}
