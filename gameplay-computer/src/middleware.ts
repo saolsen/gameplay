f1a109fe5f9168619db631c9075d472a94299a8e
steve olsen <steve.olsen@close.com>
2023-09-15 15:23:32 -0500

    rewrite it in rust

--- gameplay-computer/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-//import { currentUser } from "./auth";
-//import { db } from "./db";
-
-export const onRequest = defineMiddleware(async (_context, next) => {
-    //const user = await currentUser(context.cookies);
-    return next();
-});
