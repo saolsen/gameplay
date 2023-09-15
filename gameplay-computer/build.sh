cf13d0ecac6d84b08d6fb733657495a9edee059f
steve olsen <steve.olsen@close.com>
2023-09-15 15:50:00 -0500

    run migrations when building

--- gameplay-computer/build.sh
+++ gameplay-computer/build.sh
@@ -1,1 +1,3 @@
-echo "This is a build script running!"
+yum install postgresql
+$HOME/.cargo/bin/cargo install diesel_cli --no-default-features --features postgres
+$HOME/.cargo/bin/diesel migration run
